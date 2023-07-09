8cc0632cf891f5f985fc62c55bd8c4ab238b7590
FaMix <45107582+FaMix@users.noreply.github.com>
2023-07-09 03:38:57 +0200

    bug fixes

--- siw-movie-3-java-17/src/main/java/it/uniroma3/siw/service/FileStorageService.java
+++ siw-movie-3-java-17/src/main/java/it/uniroma3/siw/service/FileStorageService.java
@@ -3,11 +3,11 @@ package it.uniroma3.siw.service;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -32,12 +32,8 @@ public class FileStorageService {
 	@Transactional
 	public void save(MultipartFile file) {
 		try {
-			Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+			Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
 		} catch (Exception e) {
-			if (e instanceof FileAlreadyExistsException) {
-				throw new RuntimeException("A file of that name already exists.");
-			}
-
 			throw new RuntimeException(e.getMessage());
 		}
 	}
